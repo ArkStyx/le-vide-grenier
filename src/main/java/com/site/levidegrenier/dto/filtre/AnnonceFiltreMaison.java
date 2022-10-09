4f30ccc133b17bded65ab5d2ae53d5a52850996c
ArkStyx <regis.petit@gmail.com>
2022-10-09 15:30:37 +0200

    Ajout de TODO pour mieux organiser la BDD et les classes

--- src/main/java/com/site/levidegrenier/dto/filtre/AnnonceFiltreMaison.java
+++ src/main/java/com/site/levidegrenier/dto/filtre/AnnonceFiltreMaison.java
@@ -18,29 +18,32 @@ public class AnnonceFiltreMaison extends AnnonceFiltreCommun {
 	private Set<String> listeTypeMatiere;
 	private Set<String> listeColoris;
 	private Set<String> listeEtat;
-	private boolean siAnnonceDisponible;
+	private boolean siAnnonceDisponible;	// FIXME CA SERT A QUOI CONCRETEMENT ?????
+	private boolean siDonUniquement;
 	
 	
 	/*
 	FIXME CLASSE A MODIFIER
 	
+	FIXME POURQUOI NE PAS METTRE UN TYPE GLOBAL A TOUTES LES CATEGORIES ? TROUVER LES NOMS DE CATEGORIES
+	-	Meuble de : Cuisine, Salle de Bain, ...
+	-	Electromenager de : Cuisine, Salle de Bain, ...
+	-	Arts De La Table de : Assiette, Rangements, Outils De Cuisine ...
+	
+	
 	TODO A CREER - Sous-Filtre Maison.Ameublement :
-		-	Voir les annonces avec livraison également
 		-	Produit (Accessoire, Armoire, Bar, Bain & baignoire, …)
 		-	Matière (Acier, Bois, Bois massif, Bronze, …)
 		-	Couleur (Argenté, Beige, Blanc, Bleu, …)
-		-	État
 		-	TODO - A INTEGRER : Dons uniquement
 
 	TODO A CREER - Sous-Filtre	Maison.Electromenager :
-			-	TODO - A INTEGRER : Type (Cuisine, Maison et entretien, Beauté / Soin de la personne)
-			-	TODO - A INTEGRER : Produit (Appareil de massage, Brosse à dent électrique, Brosse coiffante, Epilateur, …)
-			-	État
+		-	Type (Cuisine, Maison et entretien, Beauté / Soin de la personne)
+		-	Produit (Appareil de massage, Brosse à dent électrique, Brosse coiffante, Epilateur, …)
 	
 	TODO A CREER - Sous-Filtre	Maison.Arts De La Table :
-			-	Produit (Assiette, Beurrier, Bocaux et pots, Boite de conservation et boite en métal, …)
-			-	Matière (Acrylique, Alu, Argenterie, Bambou, …)
-			-	Couleur (Argenté, Beige, Blanc, Bleu, …)
-			-	État
+		-	Produit (Assiette, Beurrier, Bocaux et pots, Boite de conservation et boite en métal, …)
+		-	Matière (Acrylique, Alu, Argenterie, Bambou, …)
+		-	Couleur (Argenté, Beige, Blanc, Bleu, …)
 	*/
 }
