5a08bc35314d9664d7dbfe6833ac1d7a0d67e25f
ArkStyx <regis.petit@gmail.com>
2022-10-12 23:35:04 +0200

    Ajout des futurs methodes a implementer

--- src/main/java/com/site/levidegrenier/controller/LeVideGrenierController.java
+++ src/main/java/com/site/levidegrenier/controller/LeVideGrenierController.java
@@ -17,10 +17,29 @@ public class LeVideGrenierController {
 	// TODO A SUPPRIMER ----------------------------------------------------------------------------------------------------
 	// TODO A SUPPRIMER ----------------------------------------------------------------------------------------------------
 	
-	// TODO A SUPPRIMER - METHODE DE TEST
-	public ResponseEntity<String> methodeDeTest() {
-		return ResponseEntity.ok("methodeDeTest");
-	}
+	/*
+	TODO A SUPPRIMER - QUELLES METHODES A PREVOIR ?
+	
+	- recherche d'annonce
+	- sauvegarder une recherche
+	- modifier une recherche
+	- supprimer une recherche
+	
+	- ajouter une annonce dans les favoris
+	- supprimer une annonce des favoris
+	
+	- poster une annonce
+	- editer une annonce
+	- supprimer une annonce
+	
+	- envoyer un message dans un fil
+	- supprimer un fil de messages
+	
+	- creer un compte
+	- modifier mot de passe
+	- ajouter une photo
+	- supprimer compte
+	*/
 	
 	// TODO A SUPPRIMER ----------------------------------------------------------------------------------------------------
 	// TODO A SUPPRIMER ----------------------------------------------------------------------------------------------------
