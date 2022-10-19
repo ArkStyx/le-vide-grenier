22e035c4ce867fd9d7566accec61cc287a9fb974
ArkStyx <regis.petit@gmail.com>
2022-10-19 23:34:02 +0200

    Ajout de tables et de TODO sur Spring Data pour generer les tables en dynamiques (evolution plus tard pour tester et apprendre)

---
+++ src/main/java/com/site/levidegrenier/dao/TodoClasseDao.java
@@ -0,0 +1,11 @@
+package com.site.levidegrenier.dao;
+
+public class TodoClasseDao {
+
+	/*
+	TODO
+	01)	CREER LES CLASSES DAO
+	02)	GENERER LES CLASSES VIA SPRING DATA https://www.baeldung.com/spring-data-jpa-generate-db-schema
+	 */
+	
+}
---
+++ src/main/java/com/site/levidegrenier/service/TodoClasseService.java
@@ -0,0 +1,12 @@
+package com.site.levidegrenier.service;
+
+public class TodoClasseService {
+
+	/*
+	TODO
+	01)	SERVICE AUTHENTIFICATION
+	02)	SERVICE RECHERCHE
+	03)	SERVICE PAIEMENT EN LIGNE
+	 */
+	
+}
