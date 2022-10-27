d20ba583561c21ceafc3225b18ee18a7abea7111
ArkStyx <regis.petit@gmail.com>
2022-10-27 21:30:55 +0200

    Modification en cours du script BDD pour synthetiser les tables

--- src/main/resources/perso/database/script_init_v05_avec_conseils_Mejdi.sql
+++ src/main/resources/perso/database/script_init_v05_avec_conseils_Mejdi.sql
@@ -81,10 +81,10 @@ CREATE TABLE lvg.tb_annonce_historique (
 	creation										TIMESTAMP NOT NULL,
 	modification									TIMESTAMP NOT NULL,
 	suppression										TIMESTAMP NOT NULL,
+	FOREIGN KEY (annonce_id)						REFERENCES lvg.tb_annonce (annonce_id),
 	FOREIGN KEY (email)								REFERENCES lvg.tb_utilisateur (email),
 	FOREIGN KEY (categorie_id)						REFERENCES lvg.tb_categorie (categorie_id),
-	FOREIGN KEY (localisation_gps_id)				REFERENCES lvg.tb_localisation_gps (localisation_gps_id),
-	FOREIGN KEY (annonce_id)						REFERENCES lvg.tb_annonce (annonce_id)
+	FOREIGN KEY (localisation_gps_id)				REFERENCES lvg.tb_localisation_gps (localisation_gps_id)
 );
 
 CREATE TABLE lvg.tb_photo (
@@ -114,6 +114,28 @@ CREATE TABLE lvg.tb_type_vente_immobiliere (
 	UNIQUE(libelle_type_vente_immobiliere)
 );
 
+CREATE TABLE lvg.tb_annonce_maison (
+	annonce_maison_id								SERIAL PRIMARY KEY,
+	si_annonce_avec_livraison						BOOLEAN NOT NULL,
+	si_disponible									BOOLEAN NOT NULL,
+	si_don											BOOLEAN NOT NULL,
+	annonce_id										INTEGER NOT NULL,
+	creation										TIMESTAMP NOT NULL,
+	modification									TIMESTAMP NOT NULL,
+	suppression										TIMESTAMP NOT NULL,
+	FOREIGN KEY (annonce_id)						REFERENCES lvg.tb_annonce (annonce_id)
+);
+
+CREATE TABLE lvg.tb_annonce_animal (
+	annonce_animal_id								SERIAL PRIMARY KEY,
+	type_animal										VARCHAR(50) NOT NULL,
+	annonce_id										INTEGER NOT NULL,
+	creation										TIMESTAMP NOT NULL,
+	modification									TIMESTAMP NOT NULL,
+	suppression										TIMESTAMP NOT NULL,
+	FOREIGN KEY (annonce_id)						REFERENCES lvg.tb_annonce (annonce_id)
+);
+
 CREATE TABLE lvg.tb_annonce_immobilier (
 	annonce_immobilier_id							SERIAL PRIMARY KEY,
 	libelle_type_bien_immobilier					VARCHAR(100) NOT NULL,
@@ -140,28 +162,6 @@ CREATE TABLE lvg.tb_annonce_location (
 	modification									TIMESTAMP NOT NULL,
 	suppression										TIMESTAMP NOT NULL,
 	FOREIGN KEY (annonce_immobilier_id)				REFERENCES lvg.tb_annonce_immobilier (annonce_immobilier_id)
-);
-
-CREATE TABLE lvg.tb_annonce_maison (
-	annonce_maison_id								SERIAL PRIMARY KEY,
-	si_annonce_avec_livraison						BOOLEAN NOT NULL,
-	si_disponible									BOOLEAN NOT NULL,
-	si_don											BOOLEAN NOT NULL,
-	annonce_id										INTEGER NOT NULL,
-	creation										TIMESTAMP NOT NULL,
-	modification									TIMESTAMP NOT NULL,
-	suppression										TIMESTAMP NOT NULL,
-	FOREIGN KEY (annonce_id)						REFERENCES lvg.tb_annonce (annonce_id)
-);
-
-CREATE TABLE lvg.tb_annonce_animal (
-	annonce_animal_id								SERIAL PRIMARY KEY,
-	type_animal										VARCHAR(50) NOT NULL,
-	annonce_id										INTEGER NOT NULL,
-	creation										TIMESTAMP NOT NULL,
-	modification									TIMESTAMP NOT NULL,
-	suppression										TIMESTAMP NOT NULL,
-	FOREIGN KEY (annonce_id)						REFERENCES lvg.tb_annonce (annonce_id)
 );
 
 CREATE TABLE lvg.tb_type_produit (
@@ -282,6 +282,90 @@ CREATE TABLE lvg.tb_recherche_location (
 	suppression										TIMESTAMP NOT NULL,
 	FOREIGN KEY (recherche_immobilier_id)				REFERENCES lvg.tb_recherche_immobilier (recherche_immobilier_id)
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- REFONTE EN COURS
+
+CREATE TABLE lvg.tb_annonce___v02 (
+	annonce_id										SERIAL PRIMARY KEY,
+	email											TEXT NOT NULL,
+	recherche_id									INTEGER NOT NULL,
+	localisation_gps_id								INTEGER NOT NULL,
+	FOREIGN KEY (email)								REFERENCES lvg.tb_utilisateur (email),
+	FOREIGN KEY (recherche_id)						REFERENCES lvg.tb_recherche (recherche_id),
+	FOREIGN KEY (localisation_gps_id)				REFERENCES lvg.tb_localisation_gps (localisation_gps_id)
+);
+
+CREATE TABLE lvg.tb_recherche___v02 (
+	recherche_id									SERIAL PRIMARY KEY,
+	categorie_id									INTEGER NOT NULL,
+	prix											INTEGER NOT NULL,
+	si_offre										BOOLEAN	NOT NULL,
+	si_annonce_urgente								BOOLEAN NOT NULL,
+	FOREIGN KEY (categorie_id)						REFERENCES lvg.tb_categorie (categorie_id)
+);
+
+CREATE TABLE lvg.tb_recherche_preferee___v02 (
+	email											TEXT NOT NULL,
+	recherche_id									INTEGER NOT NULL,
+	FOREIGN KEY (email)								REFERENCES lvg.tb_utilisateur (email),
+	FOREIGN KEY (recherche_id)						REFERENCES lvg.tb_recherche (recherche_id)
+);
+
+
+CREATE TABLE lvg.tb_annonce_historique___v02 (
+	annonce_historique_id							SERIAL PRIMARY KEY,
+	email											TEXT NOT NULL,
+	recherche_id									INTEGER NOT NULL,
+	localisation_gps_id								INTEGER NOT NULL,
+	creation										TIMESTAMP NOT NULL,
+	modification									TIMESTAMP NOT NULL,
+	suppression										TIMESTAMP NOT NULL,
+	FOREIGN KEY (email)								REFERENCES lvg.tb_utilisateur (email),
+	FOREIGN KEY (recherche_id)						REFERENCES lvg.tb_recherche (recherche_id),
+	FOREIGN KEY (localisation_gps_id)				REFERENCES lvg.tb_localisation_gps (localisation_gps_id)
+);
+
+CREATE TABLE lvg.tb_recherche_historique___v02 (
+	recherche_historique_id							SERIAL PRIMARY KEY,
+	categorie_id									INTEGER NOT NULL,
+	prix											INTEGER NOT NULL,
+	si_offre										BOOLEAN	NOT NULL,
+	si_annonce_urgente								BOOLEAN NOT NULL,
+	creation										TIMESTAMP NOT NULL,
+	modification									TIMESTAMP NOT NULL,
+	suppression										TIMESTAMP NOT NULL,
+	FOREIGN KEY (categorie_id)						REFERENCES lvg.tb_categorie (categorie_id)
+);
+
+CREATE TABLE lvg.tb_recherche_preferee_historique___v02 (
+	recherche_preferee_historique_id				SERIAL PRIMARY KEY,
+	email											TEXT NOT NULL,
+	recherche_id									INTEGER NOT NULL,
+	creation										TIMESTAMP NOT NULL,
+	modification									TIMESTAMP NOT NULL,
+	suppression										TIMESTAMP NOT NULL,
+	FOREIGN KEY (email)								REFERENCES lvg.tb_utilisateur (email),
+	FOREIGN KEY (recherche_id)						REFERENCES lvg.tb_recherche (recherche_id)
+);
+
+
+
 
 
 
